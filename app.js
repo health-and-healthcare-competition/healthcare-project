619d900840b41882fbc0107f1512eab5b43be1a5
Chialing <huchia@oregonstate.edu>
2023-02-17 10:44:51 -0800

    pharmacy tab - search function done, sort by distance not yet

--- app.js
+++ app.js
@@ -5,38 +5,93 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app = express ();            // We need to instantiate an express object to interact with the server in our code
+PORT = 9530;                      // Set a port number at the top so it's easy to change in the future
+var db = require('./database/db-connector');
+var hbs = require('hbs');
+var exphbs = require('express-handlebars');
+const { response } = require('express');
+app.engine('hbs', exphbs.engine({defaultLayout: 'main',extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', './views');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static(__dirname + '/public'));
-PORT = 9527;                      // Set a port number at the top so it's easy to change in the future
-var db = require('./database/db-connector')
-const { engine } = require('express-handlebars');
-var exphbs = require('express-handlebars');
-app.engine('.hbs', engine({extname: ".hbs"}));
-app.set('view engine', '.hbs');
 app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
+
 
 /*
     ROUTES
 */
 app.get('/', function(req,res){
+ 
     res.render('doctor');
 });
 
-app.get('/pharmacy.hbs', function(req, res){             
-    res.render('pharmacy');  
+app.get('/pharmacy', function(req, res){  
+   res.render('pharmacy');
+    
 });
 
-app.get('/doctor.hbs', function(req,res){
+app.get('/doctor', function(req,res){
     res.render('doctor');
 });
 
-app.get('/faq.hbs', function(req,res){
+app.get('/faq', function(req,res){
     res.render('faq');
 });
 
-app.get('/hospital.hbs', function(req,res){
+app.get('/hospital', function(req,res){
     res.render('hospital');
+});
+
+
+app.post('/pharmacy', function(req, res){
+    var data = req.body;
+    // handle client submit request 
+    if(req.body.act == 'Cityinput'&& req.body.dist == 0){
+        let query1 = `Select * from Pharmacies WHERE city = "${req.body.city}";`;
+        db.pool.query(query1, function(error, rows, fields){
+            if(error){
+                console.log(error)
+                res.sendStatus(400);
+            }
+            else{
+                //console.log(rows)
+                // send response back to client 
+                res.json(rows);
+            }
+        });
+    }
+    if(req.body.act == 'Cityinput'&& req.body.dist == 35){
+        let query2 = `Select * from Pharmacies;`;
+        db.pool.query(query2, function(error, rows, fields){
+            if(error){
+                console.log(error)
+                res.sendStatus(400);
+            }
+            else{
+                //console.log(rows)
+                // send response back to client 
+                res.json(rows);
+            }
+        });
+    }
+    // handle client autocomplete request 
+    if(req.body.act == 'req-source'){
+        //console.log("search: " + req.body.search)
+        let query3 = `SELECT cityName from City WHERE cityName Like "%${req.body.search}%";`;
+        db.pool.query(query3, function(error,rows,fields){
+            if(error){
+                console.log(error)
+                res.sendStatus(400);
+            }
+            else{
+                //console.log(rows)
+                // send response back to client
+                res.json(rows);
+            }
+        })
+    }
 });
 
 
---
+++ public/js/pharmacy.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+    // handle autocomplete when user input city name 
+    $('#inputCity').autocomplete({
+        source: function(req,res){
+            // send request to server side
+            $.ajax({
+                url: 'pharmacy',
+                type: 'POST',
+                dataType: 'JSON',
+                data:{
+                    act: "req-source",
+                    search: req.term
+                },
+                // successful get response from server
+                success: function(result){
+                    //console.log("success from autocomplete")
+                    res($.map(result, function(obj, key){
+                        //console.log("this is obj name " + obj.cityName)
+                        return { label: obj.cityName}
+                    }));
+                },
+                error: function(result){
+                    console.log(result)
+                }
+            });
+        },
+        minLength: 2,
+        focus: function(event, ui){
+            event.preventDefault();
+        },
+        select: function(event, ui){
+            event.preventDefault();
+            $('#inputCity').val(ui.item.label);
+        }
+    });
+
+    // handle click submit button action 
+    $('#bnt').click(function(event){
+        event.preventDefault();
+        var cityNameVal = $('#inputCity').val();
+        var distanceVal = $('#distances').val();
+    
+        if(cityNameVal==''){
+            alert('Please provide a city name!')
+            return
+        }
+    
+        var obj = {
+            city: cityNameVal,
+            act: "Cityinput",
+            dist: distanceVal
+        }
+        // send request to server side
+        $.ajax({
+            url: '/pharmacy',
+            type: 'POST',
+            dataType: 'JSON',
+            data: obj,
+            // successful get response from server
+            success:function(result){
+                //console.log("success")
+                //console.log(result)
+                // find the matched result 
+                if(result.length>0){
+                    $('#count-here').html("Found "+ result.length + " in-network pharmacies.");
+                    $('#table-here').removeAttr('hidden');
+                    var source = $('#resultTemplate').html();
+                    var template = Handlebars.compile(source); 
+                    var html = template({data: result});
+                    $('#table-here').html(html);
+                }
+                // no matched result
+                else if(result.length==0){
+                    $('#count-here').html("Found "+ result.length + " in-network pharmacies.");
+                    $('#table-here').attr('hidden',true);
+                }
+            },
+            error: function(result){
+                console.log(result)
+            }
+        })
+    })
+});
